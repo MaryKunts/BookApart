6c09ed8d9b545c21a496ff865db79ec7e7fe8cc3
MaryKunts <96074418+MaryKunts@users.noreply.github.com>
2024-09-27 17:25:54 +0200

    Added header,footer and content components (#1)
    * Added header,footer and content components
    
    * Fixed mistakes
    
    * Fixed mistakes 2.0
    
    * Fixed mistakes 2.1

---
+++ src/components/content/content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./content.module.scss";
+
+const Content = () => {
+  return <div className={styles.wrapper}>some content</div>;
+};
+
+export default Content;
