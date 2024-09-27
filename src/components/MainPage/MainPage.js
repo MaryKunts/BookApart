816435a36909fe281bd617ece2fb630f63d029ff
Maryia <marykunc@gmail.com>
2024-09-27 17:12:43 +0200

    Fixed mistakes 2.0

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 import Header from "../header/header";
-import Content from '../content/content';
-import Footer from '../footer/footer'
+import Content from "../content/content";
+import Footer from "../footer/footer";
 
 import styles from "./MainPage.module.scss";
 
