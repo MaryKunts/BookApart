33ece627ca235969392fb4d4bd520100ebbf633d
Maryia <marykunc@gmail.com>
2024-09-30 15:43:24 +0200

    Created array with accomodation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouseChimney,
@@ -13,28 +14,40 @@ const Header = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.iconWrapper}>
-        <button className={styles.iconButton}>
+        <Button className={styles.iconButton}>
           <FontAwesomeIcon
             icon={faHouseChimney}
             size="2xl"
             style={{ color: "#7c60d2" }}
           />
           <div className={styles.iconButtonTitle}>BookApart</div>
-        </button>
+        </Button>
       </div>
       <div className={styles.settingsWrapper}>
         <div className={styles.rentOutButtonWrapper}>
-          <button className={styles.rentOutButton}>
-            Сдать жильё на BookApart
-          </button>
+          <Button className={styles.rentOutButton}>
+            <FontAwesomeIcon
+              className={styles.rentOutButtonIcon}
+              icon={faMoneyBill}
+              size="xl"
+              style={{ color: "#4a4c4f" }}
+            />
+            <div className={styles.rentOutButtonTitle}>
+              Сдать жильё на BookApart
+            </div>
+          </Button>
         </div>
         <div className={styles.languageButtonWrapper}>
-          <button className={styles.languageButton}>
-            <FontAwesomeIcon icon={faGlobe} size="xl" />
-          </button>
+          <Button className={styles.languageButton}>
+            <FontAwesomeIcon
+              icon={faGlobe}
+              style={{ color: "#4a4c4f" }}
+              size="xl"
+            />
+          </Button>
         </div>
         <div className={styles.profileButtonWrapper}>
-          <button className={styles.profileButton}>
+          <Button className={styles.profileButton}>
             <div className={styles.profileMenu}>
               <FontAwesomeIcon
                 icon={faBars}
@@ -49,7 +62,7 @@ const Header = () => {
                 style={{ color: "#4a4c4f" }}
               />
             </div>
-          </button>
+          </Button>
         </div>
       </div>
     </div>
